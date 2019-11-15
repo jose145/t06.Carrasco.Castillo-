33374a41ba183170c83e8413cd9c7293bba8cffc
jose145 <jcastilloj@unprg.edu.pe>
2019-11-15 15:02:06 -0500

    se creo el archivo condicional13_simple.py

---
+++ Castillo/condicionales_simples/condicional13_simple.py
@@ -0,0 +1,20 @@
+import os
+
+#Calcular el area del rombo
+
+#Declaracion de variables
+diagonal_menor,diagonal_mayor= 0,0
+area=0.0,0.0,0.0
+
+#input
+diagonal_menor= float(os.sys.argv[1])
+diagonal_mayor= float(os.sys.argv[2])
+
+#procesing
+area= (diagonal_menor*diagonal_mayor)/2
+
+#output
+#Si el area supera los 100 metros cuadrados mostrar "Gran rombo"
+if (area >  100):
+    print("Gran rombo")
+#fin_if
