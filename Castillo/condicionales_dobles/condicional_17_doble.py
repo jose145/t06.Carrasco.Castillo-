8b3958ec2e23e139c0db8d942b4af00dd3db81b7
jose145 <jcastilloj@unprg.edu.pe>
2019-11-15 15:14:47 -0500

    se creo el archivo condicional_l7_doble.py

---
+++ Castillo/condicionales_dobles/condicional_17_doble.py
@@ -0,0 +1,27 @@
+import os
+
+#Calcular energia potencial gravitatoria de un cuerpo
+
+#Declaracion de variables
+masa,gravedad,altura= 0,0,0
+energia_potencial_gravitatoria=0
+
+#input
+masa= int(os.sys.argv[1])
+gravedad= int(os.sys.argv[2])
+altura= int(os.sys.argv[3])
+
+#procesing
+energia_potencial_gravitatoria= (masa*gravedad*altura)
+
+#output
+#Si la energia supera 150 joules mostrar "Mucha energia"
+if (energia_potencial_gravitatoria >  150):
+    print("Mucha energia")
+
+#Si la energia no supera 150 joules mostrar "Energia insuficiente"
+if (energia_potencial_gravitatoria <  150):
+    print("Energia insuficiente")
+
+#fin_if
+
